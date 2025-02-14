4d61ca4c7e858c5fec114782c2a3f3672c60e59b
Jeanne-ONGTIGA <jeanneongtiga@gmail.com>
2025-02-14 18:12:42 +0100

    Ajout

--- src/05/TAILW2.tsx
+++ src/05/TAILW2.tsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
 import { BiCommentDetail } from "react-icons/bi";
 import { CiBookmarkCheck } from "react-icons/ci";
+import { FaHeart } from "react-icons/fa";
 import { IoIosHeartEmpty, IoIosSend, IoMdHeart } from "react-icons/io";
 import { LuSendHorizontal } from "react-icons/lu";
 
 export default function TAILW2 () {
+    const [liked, setLiked] = useState(false);
     return(
         <div className="bg-blue-100 w-screen h-screen flex items-center place-content-center">
         <div className="bg-white rounded shadow w-44 h-72 flex flex-col">
@@ -11,9 +14,16 @@ export default function TAILW2 () {
             <div className="bg-yellow-100 flex-1"></div>
             <div className="h-8 flex item-center px-2">
                 <div className="flex w-1/2 space-x-2">
-                <IoIosHeartEmpty /> 
-                <IoMdHeart />
-                <IoIosSend />
+                { liked ?
+                <button className="cursor-pointer" onClick={()=> setLiked(false)}>
+                     <IoIosHeartEmpty className="text-red-500" />  
+                </button>
+                :
+                <button className="cursor-pointer" onClick={()=> setLiked(true)}>
+                    <IoMdHeart className="text-red-500"/>
+                </button>
+                }
+                <IoIosSend className=""/>
                 </div>
                 <div className="flex place-contet-end w-1/2">
                 <CiBookmarkCheck />
